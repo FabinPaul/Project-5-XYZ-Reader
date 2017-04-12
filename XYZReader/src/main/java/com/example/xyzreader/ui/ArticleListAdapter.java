654126107b7a4a5b2ba25785792f04cfedae5412
Fabin Paul <fabinpaul92@gmail.com>
2017-04-12 18:09:57 +0530

    fix: Added material color and animation
    Added material color for primary and accent colors. Added animation for
    recyclerview items.

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListAdapter.java
@@ -0,0 +1,150 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.xyzreader.R;
+import com.example.xyzreader.data.ArticleLoader;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Fabin Paul, Eous Solutions Delivery on 4/12/2017 11:07 AM.
+ */
+
+public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.ViewHolder> {
+
+    private static final String TAG = ArticleListAdapter.class.getSimpleName();
+
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss");
+    // Use default locale format
+    private SimpleDateFormat outputFormat = new SimpleDateFormat();
+    // Most time functions can only handle 1902 - 2037
+    private GregorianCalendar START_OF_EPOCH = new GregorianCalendar(2, 1, 1);
+
+    private Cursor mCursor;
+    private final Context mContext;
+    private OnClickListener mOnClickListener;
+    private float mAnimationOffset;
+
+    public void setOnClickListener(@NonNull OnClickListener onClickListener) {
+        mOnClickListener = onClickListener;
+    }
+
+    interface OnClickListener {
+        public void onClick(ViewHolder viewHolder, int position);
+    }
+
+    public ArticleListAdapter(@NonNull Context context, Cursor cursor) {
+        mContext = context;
+        mCursor = cursor;
+        mAnimationOffset = mContext.getResources().getDimensionPixelSize(R.dimen.offset_y);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        mCursor.moveToPosition(position);
+        return mCursor.getLong(ArticleLoader.Query._ID);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_article, parent, false);
+        final ViewHolder vh = new ViewHolder(view);
+//        animateViewsIn(vh);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mOnClickListener != null)
+                    mOnClickListener.onClick(vh, vh.getAdapterPosition());
+            }
+        });
+        return vh;
+    }
+
+    private Date parsePublishedDate() {
+        try {
+            String date = mCursor.getString(ArticleLoader.Query.PUBLISHED_DATE);
+            return dateFormat.parse(date);
+        } catch (ParseException ex) {
+            Log.e(TAG, ex.getMessage());
+            Log.i(TAG, "passing today's date");
+            return new Date();
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        mCursor.moveToPosition(position);
+        holder.titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
+        Date publishedDate = parsePublishedDate();
+        if (!publishedDate.before(START_OF_EPOCH.getTime())) {
+
+            holder.subtitleView.setText(Html.fromHtml(
+                    DateUtils.getRelativeTimeSpanString(
+                            publishedDate.getTime(),
+                            System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                            DateUtils.FORMAT_ABBREV_ALL).toString()
+                            + "<br/>" + " by "
+                            + mCursor.getString(ArticleLoader.Query.AUTHOR)));
+        } else {
+            holder.subtitleView.setText(Html.fromHtml(
+                    outputFormat.format(publishedDate)
+                            + "<br/>" + " by "
+                            + mCursor.getString(ArticleLoader.Query.AUTHOR)));
+        }
+        holder.thumbnailView.setImageUrl(
+                mCursor.getString(ArticleLoader.Query.THUMB_URL),
+                ImageLoaderHelper.getInstance(mContext).getImageLoader());
+        holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
+    }
+
+//    private void animateViewsIn(ViewHolder viewHolder) {
+//        View view = viewHolder.itemView;
+//        Interpolator interpolator =
+//                AnimationUtils.loadInterpolator(mContext, android.R.interpolator.linear_out_slow_in);
+//
+//        view.setVisibility(View.VISIBLE);
+//        view.setTranslationY(mAnimationOffset);
+//        view.setAlpha(0.85f);
+//        // then animate back to natural position
+//        view.animate()
+//                .translationY(0f)
+//                .alpha(1f)
+//                .setInterpolator(interpolator)
+//                .setDuration(1000L)
+//                .start();
+//        // increase the offset distance for the next view
+//        mAnimationOffset *= 1.5f;
+//    }
+
+    @Override
+    public int getItemCount() {
+        return mCursor.getCount();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public DynamicHeightNetworkImageView thumbnailView;
+        public TextView titleView;
+        public TextView subtitleView;
+
+        public ViewHolder(View view) {
+            super(view);
+            thumbnailView = (DynamicHeightNetworkImageView) view.findViewById(R.id.thumbnail);
+            titleView = (TextView) view.findViewById(R.id.article_title);
+            subtitleView = (TextView) view.findViewById(R.id.article_subtitle);
+        }
+    }
+}
